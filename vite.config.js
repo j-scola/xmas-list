89380b2951eb9f7d588137528b881413734f613a
James Scolamieri <james.scolamieri@gmail.com>
2024-11-20 13:18:52 -0800

    fix(netlify): ensure app works in netlify subdirectory

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     open: true, // Automatically open the browser
     port: 3000, // Custom port (default is 5173)
